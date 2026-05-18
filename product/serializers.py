8aebe1dbb773fc37398c7e74d52838f9404b706d
ibrohim0811 <dangercoder046@gmail.com>
2026-05-18 16:15:24 +0500

    githubdagi issueslardagi vazifalar va make buyruqlari qoshildi

--- product/serializers.py
+++ product/serializers.py
@@ -1,9 +1,43 @@
 from rest_framework.serializers import ModelSerializer, Serializer
 
-from .models import Category, Book
+from .models import Category, Book, BookImage
 
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug', 'category_image']
+
+
+
+class CategoryUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name', 'category_image']
+
+
+
+
+class BookImageSerializer(ModelSerializer):
+    class Meta:
+        model = BookImage
+        fields = ['id', 'image']
+
+
+
+class BookSerializer(ModelSerializer):
+
+    book_image = BookImageSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = ['id', 'name', 'slug', 'price', 'book_image', 'category', 'info', 'author', 'views']
+
+
+class CategoryDetailSerializer(ModelSerializer):
+
+    books = BookSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug', 'category_image', 'books']
