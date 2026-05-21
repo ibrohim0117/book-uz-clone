07fc4183ab06c802389594d8e7c53197383afb26
ibrohim0811 <dangercoder046@gmail.com>
2026-05-21 18:32:49 +0500

    users/models.py social network classi ga update kiritildi

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class SocialNetwork(models.Model):
-    url = models.CharField(max_length=500)
+    title = models.CharField(max_length=250, blank=True, null=True)
+    url = models.CharField(max_length=500, blank=True, null=True)
 
 
 
@@ -19,7 +20,7 @@ class Users(AbstractUser):
     role = models.CharField(max_length="15", choices=RoleChoices.choices, default=RoleChoices.CLIENT)
     about = models.TextField(blank=True, null=True)
     theme_dark = models.BooleanField(default=False)
-    social_network = models.ForeignKey(SocialNetwork, on_delete=models.CASCADE, related_name='social_network')
+    social_network = models.ForeignKey(SocialNetwork, on_delete=models.CASCADE, related_name='social_network', null=True, blank=True)
     
 
 
